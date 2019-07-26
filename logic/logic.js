0fdd8a9561324ecf58f5401490a2004b228f0610
riverandeye <riverandeye@gmail.com>
2019-07-26 19:02:36 +0900

    2nd week project commit

---
+++ logic/logic.js
@@ -0,0 +1,46 @@
+// recipe.json 파일에서 읽어들어온 레시피 정보
+
+
+const getAllIngredient = function(recipes){
+    return recipes.reduce((acc,val)=>{acc = [...acc, ...val.ingredients]; return acc}, []);
+}
+
+const SavetoFile = function(path, recipes){
+
+}
+
+const addRecipes = function(addObject, recipes){
+    return [...recipes, addObject];
+}
+
+const calculateCount = function(current, menu){
+    let count = 0;
+    for(let ingredient of current){
+        if(menu.ingredients.includes(ingredient)){
+            count++;
+        }
+    }
+    menu.matchsize = count;
+    return menu;
+ }
+
+const getMaximumMatches = function(current, recipes){
+
+    recipes = recipes.reduce((acc,obj)=>{
+        acc.push(calculateCount(current, obj));
+        return acc;
+    }, []);
+
+    recipes.sort((a,b)=>{
+        return a.matchsize < b.matchsize
+    })
+
+    if(recipes.length >= 6) return recipes.slice(0,6);
+    else return recipes
+}
+
+module.exports={
+    getMaximumMatches : getMaximumMatches,
+    calculateCount : calculateCount,
+
+}
---
+++ record.md
@@ -0,0 +1,18 @@
+
+## 파일트리
+
+- ./logic
+    - logic.js - 메인 기능을 실행하는 부분
+    - data.json - 레시피 데이터 [ {recipe}, {recipe}, , ,]
+    - ingredients.js - 재료 데이터 [ "재료", "재료", , ,]
+- ./router
+    - app.js - 라우터 파일
+
+- ./views
+    - html(ejs) 파일들
+
+
+## requirements
+- "body-parser": "^1.19.0",
+- "ejs": "^2.6.2",
+- "express": "^4.17.1"
