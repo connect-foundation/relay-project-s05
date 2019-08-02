ef3370e5e37b6a4480fd7ae12f3ceabba520161b
KimJye <eoeogudgud@naver.com>
2019-08-02 18:46:55 +0900

    add 주석

--- logic/croll.js
+++ logic/croll.js
@@ -8,6 +8,9 @@ const options = {
   uri: `http://www.10000recipe.com/recipe/list.html?q=${enCode}`
 };
 
+// 만개의 레시피에서 크롤링을 한다.
+// 예를 들어, 삼겹살을 만개의 레시피 사이트에서 검색을하면 여러개의 삼겹살 레시피 소개글들이 나오는데
+// 거기서 맨 처음에 소개글의 링크에 다시 접속하여, 해당 소개글의 레시피를 얻는다.
 request(options, function(err, res, html) {
   const $ = cheerio.load(html);
 
@@ -22,10 +25,11 @@ request(options, function(err, res, html) {
     let find = arr[0].children[0].parent;
 
     let result = "";
-    for (let i = 0; i < find.length; ++i) {
-      result += `${find.children[i].chidren[0].data}\n`;
-    }
-    // console.log(arr[0].children[0].parent.children[3].children[0].data);
+    // for (let i = 0; i < find.length; ++i) {
+    //   result += `${find.children[i].chidren[0].data}\n`;
+    // }
+
+    console.log(arr[0].children[0].parent.children[3].children[0].data);
     //console.log(result);
   });
 });
